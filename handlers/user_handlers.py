33749dee38a8db1a4d1ab3dabdeed48cd50d2c03
root <lseirina87@gmail.com>
2024-04-10 22:16:06 +0300

    Added router

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,13 +1,16 @@
+from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 from lexicon.lexicon import LEXICON_RU
 
+router = Router()
 
-@dp.message(CommandStart())
+
+@router.message(CommandStart())
 async def process_star_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'])
 
 
-@dp.message(Command(commands='help'))
+@router.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'])

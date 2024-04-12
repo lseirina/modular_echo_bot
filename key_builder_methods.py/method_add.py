72297d52760690ebeb2b8d08a44fe90f21101a61
root <lseirina87@gmail.com>
2024-04-12 22:11:26 +0300

    Added method keyboard add

--- key_builder_methods.py/method_add.py
+++ key_builder_methods.py/method_add.py
@@ -1,11 +1,10 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import Message, KeyboardButton
 from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, Message
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 BOT_TOKEN = '6883498485:AAGtOZFurG3T-H2oDNwhQcUqeUzlMfqcJHE'
-
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
@@ -13,20 +12,21 @@ buttons_1: list[KeyboardButton] = [
     KeyboardButton(text=f'Button {i + 1}') for i in range(6)
 ]
 buttons_2: list[KeyboardButton] = [
-    KeyboardButton(text=f'Button {i + 7}') for i in range(3)
+    KeyboardButton(text=f'Button {i + 7}') for i in range(10)
 ]
+
 kb_builder = ReplyKeyboardBuilder()
-kb_builder.row(*buttons_1, width=4)
-kb_builder.row(*buttons_2, width=2)
+kb_builder.row(*buttons_1, width=3)
+kb_builder.add(*buttons_2)
 
 
 @dp.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(
-        text='These are buttons.',
+        text='These are buttons',
         reply_markup=kb_builder.as_markup(resize_keyboard=True)
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     dp.run_polling(bot)

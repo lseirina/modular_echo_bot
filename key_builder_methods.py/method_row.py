929106c7855931e6220eb8cba2c3617f32662921
root <lseirina87@gmail.com>
2024-04-12 21:44:35 +0300

    Added row keyboard method

---
+++ key_builder_methods.py/method_row.py
@@ -0,0 +1,28 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import KeyboardButton, Message
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+
+BOT_TOKEN = '6883498485:AAGtOZFurG3T-H2oDNwhQcUqeUzlMfqcJHE'
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+buttons: list[KeyboardButton] = [
+    KeyboardButton(text=f'Button {i + 1}') for i in range(10)
+]
+kb_builder = ReplyKeyboardBuilder()
+kb_builder.row(*buttons, width=4)
+
+
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(
+        text='These are buttons.',
+        reply_markup=kb_builder.as_markup(resize_keyboard=True)
+    )
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
